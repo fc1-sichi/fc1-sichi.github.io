6247b1f4d5ae17ad030487d993101136515d10b7
Sichi <fc1.sichi@gmail.com>
2024-08-25 10:20:24 -0700

    Added alti_lemonz

--- instagram.js
+++ instagram.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const embeds = [
+        'https://www.instagram.com/p/C9isHWJsdVR/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==',
         'https://www.instagram.com/p/C903Cj_tjNn/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==',
         'https://www.instagram.com/p/CkTcJGnLJcr/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==',
         'https://www.instagram.com/p/Cwp1kURSdTf/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==',
